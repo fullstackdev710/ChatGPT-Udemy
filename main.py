a93908c97b445750b21f2a6465d8f0c601ecca13
fullstackdev710 <fullstack710dev@gmail.com>
2023-04-28 22:34:10 -0400

    Build a ChatGPT Clone using OepnAI API

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
+from flask import Flask, request, render_template
 import openai
 
+app = Flask(__name__)
+
 # Use OpenAI API Key
-openai.api_key = "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"
-# Ask the user for question
-question = input("What would you like to ask OpenAI?")
+openai.api_key = "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"
 
-response = openai.Completion.create(
-    engine="text-davinci-003",
-    prompt=question,
-    max_tokens=1024,
-    n=1,
-    stop=None,
-    temperature=0.8,
-)
 
-answer = response["choices"][0]["text"]
+@app.route("/")
+def index():
+    return render_template(("index.html"))
 
-print("OpenAI: " + answer)
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=userText,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=1,
+    )
+    answer = response["choices"][0]["text"]
+
+    return str(answer)
+
+
+if __name__ == "__main__":
+   #  app.run(host="127.0.0.1", port=5001, debug=True)
+    app.run()
